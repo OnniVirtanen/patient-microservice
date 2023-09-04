34a011ea6759b8d32719c5fb226e7c624247419c
Onni Virtanen <onni.virtanen5@gmail.com>
2023-09-04 22:08:08 +0300

    added javadoc

--- src/main/java/com/patient/patient/model/Constants.java
+++ src/main/java/com/patient/patient/model/Constants.java
@@ -1,7 +1,17 @@
 package com.patient.patient.model;
 
+/**
+ * Provides constant values that are used throughout the application.
+ */
 public abstract class Constants {
 
+    /**
+     * The current version of the API.
+     */
     public static final String API_VERSION = "v1";
+
+    /**
+     * The base API path for accessing patient-related endpoints.
+     */
     public static final String PATIENT_CONTROLLER_API_PATH = "api/" + API_VERSION + "/patients";
 }
